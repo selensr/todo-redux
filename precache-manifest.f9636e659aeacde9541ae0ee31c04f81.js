0966e53d4028e4279c6299b03e2d9d6e3cfbe3fc
selensr <sr.selenn@gmail.com>
2019-12-07 15:05:08 +0300

    Updates

---
+++ precache-manifest.f9636e659aeacde9541ae0ee31c04f81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88e80201b1ba2f53ab465aab95529511",
+    "url": "/todo-redux/index.html"
+  },
+  {
+    "revision": "58c130294400d52c5676",
+    "url": "/todo-redux/static/css/2.07e04f06.chunk.css"
+  },
+  {
+    "revision": "740205cdf4ceb07c2aa0",
+    "url": "/todo-redux/static/css/main.66deca54.chunk.css"
+  },
+  {
+    "revision": "58c130294400d52c5676",
+    "url": "/todo-redux/static/js/2.7780f235.chunk.js"
+  },
+  {
+    "revision": "740205cdf4ceb07c2aa0",
+    "url": "/todo-redux/static/js/main.12ad1603.chunk.js"
+  },
+  {
+    "revision": "06f785899570f68a78fb",
+    "url": "/todo-redux/static/js/runtime-main.54983173.js"
+  }
+]);
